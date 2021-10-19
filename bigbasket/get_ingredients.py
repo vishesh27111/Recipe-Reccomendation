ecdff9182f410febdba5d4282189c099bb24b53a
Darshan62 <dmaj1961319@gmail.com>
2021-10-19 15:59:13 +0530

    json issue solved

--- bigbasket/get_ingredients.py
+++ bigbasket/get_ingredients.py
@@ -5,6 +5,6 @@ details = {"item": "u", "quantity": "1"}
 
 response = requests.put(BASE + "/self_order/shopping_list/6", details)
 
-print(response.json())
+# print(response.json())
 
 print(requests.get(BASE + "/self_order/shopping_list/6").json())
