f1f082fcad625f009864dd875127c87511dbf4a2
Darshan62 <dmaj1961319@gmail.com>
2021-10-19 14:51:06 +0530

    first commit demo

--- bigbasket/get_ingredients.py
+++ bigbasket/get_ingredients.py
@@ -2,6 +2,7 @@ import requests
 BASE = 'http://127.0.0.1:5000/'
 
 details = {'item': 'tomato', 'quantity': '1'}
+
 response = requests.put(BASE + "/self_order/shopping_list/1", details)
 
 print(requests.get(BASE + '/self_order/shopping_list/1').json())
